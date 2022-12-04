dd874675bd016e001e558c1df40c90a2d8301e57
juniorTeamProject <matanka7@ac.sce.ac.il>
2022-12-04 17:40:13 +0200

    add questionary for junior signUp

--- js/register/register.js
+++ js/register/register.js
@@ -1,6 +1,22 @@
 
+//FireBase
+const firebaseConfig = {
+  apiKey: "AIzaSyCYZ9IilLKt4bZUOid185CDfwzuYMDv6yE",
+  authDomain: "junior-3c64f.firebaseapp.com",
+  projectId: "junior-3c64f",
+  storageBucket: "junior-3c64f.appspot.com",
+  messagingSenderId: "1003928300183",
+  appId: "1:1003928300183:web:ed8b6aa96a339a048257d2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+
+
 // choose image in signup from file
-   function pickImage(input) {    
+function pickImage(input) {    
     document.querySelector('.logoTxt').innerHTML = '';  // clean logo txt
     if (input.files && input.files[0]) {
       var reader = new FileReader();
@@ -17,3 +33,4 @@
   
    
   
+
---
+++ models/User.js
@@ -0,0 +1,14 @@
+
+class User{
+constructor (name,mail,password){
+    this.name = name;
+    this.mail = mail;
+    this.password = password;
+
+    
+} 
+ 
+
+
+}
+
