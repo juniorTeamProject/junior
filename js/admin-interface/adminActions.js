6b17ec37c25c41948eaa01de2218d90946effd43
Omer Cohen <omcl97@gmail.com>
2023-01-11 18:29:35 +0200

    fix bugs

--- js/admin-interface/adminActions.js
+++ js/admin-interface/adminActions.js
@@ -1,8 +1,3 @@
-///---------------Cant back if not log in---------------------
-if(currentUser.Name == null && window.location.href != "http://127.0.0.1:5500/screens/home.html"){
-    window.location.assign("/screens/home.html");
- }
- ///---------------Cant back if not log in---------------------
 
 let currentUser = JSON.parse(localStorage.getItem('currentUser'));
 let allUsers_arr = JSON.parse(localStorage.getItem('allUsers_arr')) ||[];  // all users
